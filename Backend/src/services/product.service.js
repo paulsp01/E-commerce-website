a2dc680e656d49c3485bbbe87260704973a7e8a5
swarnalee paul <paulswarnalee01@gmail.com>
2025-01-28 07:27:41 +0530

    remove debug statement

--- Backend/src/services/product.service.js
+++ Backend/src/services/product.service.js
@@ -89,92 +89,9 @@ module.exports.findProductById=async (productId)=>{
     return product
 }
 
-// module.exports.getAllProducts=async (reqQuery)=>{
-//     let {category,color,size,minPrice,maxPrice,minDiscount,sort ,stock,pageNumber,pageSize} = reqQuery
-
-//     // pageNumber = Math.max(1, pageNumber); // Ensure pageNumber is at least 1
-//     // pageSize = pageSize || 10
-
-//     pageNumber = reqQuery.pageNumber ? Math.max(1, parseInt(reqQuery.pageNumber)) : 1;
-// pageSize = reqQuery.pageSize ? parseInt(reqQuery.pageSize) : 10;
-
-
-//     let query= productModel.find().populate("category")
-
-
-//     if(category){
-//         const existCategory=await categoryModel.findOne({name:category})
-//         if(existCategory){
-//             query=query.where("category").equals(existCategory._id)
-//         }else{
-//             return {content:[],currentPage:pageNumber,totalPages:0}
-//         }
-//     }
-//     if(color){
-//         const colorSet=new Set(color.split(",").map(color=>color.trim().toLowerCase()))
-
-//         const colorRegex=colorSet.size>0?new RegExp([...colorSet].join("|"),"i"):null
-
-//         query=query.where("color").regex(colorRegex)
-//     }
-
-//     if(size){
-//         const sizeSet=new Set(size)
-//         query=query.where("size.name").in([...sizeSet])
-
-//     }
-
-//     if(minPrice && maxPrice){
-//     query=query.where("discountedPrice").gte(minPrice).lte(maxPrice)
-//     }
-
-//     if(minDiscount){
-//         query=query.where("discountPersent").gte(minDiscount)
-//     }
-
-//     if(stock){
-//         if(stock=='in_stock'){
-//             query=query.where("quantity").gte(1)
-//         }else if(stock==='out_of_stock'){
-//             query=query.where("quantity").lte(0);
-//         }
-//     }
-//     if(sort){
-//         const sortDirection=sort=='price_high'?-1:1
-//         query=query.sort({discountedPrice:sortDirection})
-//     }
-
-//     console.log("Query:", query);
-
-//     try {
-       
-//         const totalProducts=await productModel.countDocuments(query)
-       
-
-//         if (totalProducts === 0) {
-//             return { content: [], currentPage: pageNumber, totalPages: 0 };
-//         }
-//         const skip=(pageNumber-1)*pageSize
-
-//         query=query.skip(skip).limit(pageSize)
-
-//         const products=await query.exec();
-        
-
-//         const totalPages=Math.ceil(totalProducts/pageSize)
-
-//         return {content:products,currentPage:pageNumber,totalPages:totalPages}
-//     } catch (error) {
-//         console.error("Error fetching products:", error);
-//         throw new Error("Error fetching products");
-//     }
-
-// }
-
 module.exports.getAllProducts = async (reqQuery) => {
     let { category, pageNumber, pageSize,color,size,minPrice,maxPrice,minDiscount,stock,sort } = reqQuery;
-    console.log("minPrice:", minPrice);
-    console.log("maxPrice:", maxPrice);
+   
     pageNumber = reqQuery.pageNumber ? Math.max(1, parseInt(reqQuery.pageNumber)) : 1;
     pageSize = reqQuery.pageSize ? parseInt(reqQuery.pageSize) : 10;
   
@@ -189,22 +106,21 @@ module.exports.getAllProducts = async (reqQuery) => {
         return { content: [], currentPage: pageNumber, totalPages: 0 };
       }
     }
-    console.log("Query after category:", query.getQuery());
+   
 
     if (color) {
         const colorSet = new Set(color.split(",").map(color => color.trim().toLowerCase()));
         const colorRegex = colorSet.size > 0 ? new RegExp([...colorSet].join("|"), "i") : null;
         query = query.where("color").regex(colorRegex);
       }
-      console.log("Query after color:", query.getQuery());
+     
 
 
       if (size) {
         const sizeSet = new Set(size);
         query = query.where("size.name").in([...sizeSet]);
       }
-      console.log("Query after size:", query.getQuery());
-
+    
 
       if (minPrice !== undefined && maxPrice !== undefined && minDiscount !== undefined) {
         query = query.where({
@@ -217,7 +133,7 @@ module.exports.getAllProducts = async (reqQuery) => {
           }
         });
       }
-      console.log("Query after combined conditions:", query.getQuery());
+      
 
 
     
@@ -228,42 +144,29 @@ module.exports.getAllProducts = async (reqQuery) => {
       query = query.where("quantity").lte(0);
     }
   }
-  console.log("Query after stock:", query.getQuery());
-
+ 
   if (sort) {
     const sortDirection = sort == 'price_high' ? -1 : 1;
     query = query.sort({ discountedPrice: sortDirection });
   }
-  console.log("Query after sort:", query.getQuery());
-
-
-    
-    // Log the final query conditions and options
-    console.log("Final Query Conditions:", query.getQuery());
-    console.log("Query Options:", query.getOptions());
-    console.log("Page Number:", pageNumber);
-    console.log("Page Size:", pageSize);
+  
   
     try {
       const totalProducts = await productModel.countDocuments(query);
-      console.log("Total Products:", totalProducts);
+      
       if (totalProducts === 0) {
         return { content: [], currentPage: pageNumber, totalPages: 0 };
       }
       const skip = (pageNumber - 1) * pageSize;
-      console.log("Skip:", skip);
-      console.log("Limit:", pageSize);
+     
       query = query.skip(skip).limit(pageSize);
   
-      try {
+    
         const products = await query.exec();
-        console.log("Products:", products);
+       
         const totalPages = Math.ceil(totalProducts / pageSize);
         return { content: products, currentPage: pageNumber, totalPages: totalPages };
-      } catch (queryError) {
-        console.error("Error executing query:", queryError);
-        throw new Error("Error executing query");
-      }
+      
     } catch (error) {
       console.error("Error fetching products:", error);
       throw new Error("Error fetching products");
