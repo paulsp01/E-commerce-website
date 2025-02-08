59c8cafb2777978705e0c2ff5e6d46cfb40a6d2e
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-08 14:13:59 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, 
+  },
+
+
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173, // Use Render's PORT or default to 5173
+  },
 })
