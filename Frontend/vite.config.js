38f656e9bf9be700f458660a23dc2f61f02cbd76
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-08 19:04:01 +0530

    ommited

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,26 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
   build: {
-    chunkSizeWarningLimit: 1024, // Reduce chunk size limit to avoid big files
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Split large dependencies into separate chunks
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 2000, 
   },
-
   server: {
-    host: '0.0.0.0', // Allow external access
-    port: 5173, // Use Render’s assigned port
-  },
-
-  optimizeDeps: {
-    exclude: ['@mui/material', '@mui/icons-material', 'react-redux'], // Replace with heavy dependencies if needed
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['e-commerce-website-4-rjrb.onrender.com'], // Add your Render domain
   },
 })
