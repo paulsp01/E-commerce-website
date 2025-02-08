092b21dcdac93b5c34fde7c485683b5cf9ae40c1
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-08 22:06:11 +0530

    latest update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,16 +5,17 @@ import './index.css';
 import App from './App.jsx';
 import {Provider} from "react-redux";
 import { store } from './State/Store.js';
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
     <App />
 
     </Provider>
     
-    </BrowserRouter>
+    </HashRouter>
       
    
   </StrictMode>
