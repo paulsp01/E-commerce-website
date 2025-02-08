c0ddbfff885309cb6f010c16dc559a84a5e07a0d
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-08 20:37:17 +0530

    update frontend url

--- Frontend/src/config/ApiConfig.js
+++ Frontend/src/config/ApiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL="https://e-commerce-website-7-lo97.onrender.com/"
+export const API_BASE_URL="https://e-commerce-website-7-lo97.onrender.com"
 const jwt=localStorage.getItem("jwt")
 
 
