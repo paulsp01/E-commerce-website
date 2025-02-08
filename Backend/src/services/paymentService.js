b9174ce87f8d4f373291a80086f393a9ed780a73
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-08 22:41:38 +0530

    vercel url

--- Backend/src/services/paymentService.js
+++ Backend/src/services/paymentService.js
@@ -19,7 +19,7 @@ module.exports.createPaymentLink = async (orderId) => {
         email: true,
       },
       reminder_enable: true,
-      callback_url: `https://e-commerce-website-6-jkju.onrender.com/payment/${orderId}`,
+      callback_url: `https://e-commerce-website-79zu-4ouz2ivvu-swarnalee-pauls-projects.vercel.app/payment/${orderId}`,
       callback_method: "get",
     };
    
