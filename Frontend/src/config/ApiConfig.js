255730d94cf6c8c6e58bb1f6acb3734bcd0ae50b
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-09 14:59:28 +0530

    new commit

--- Frontend/src/config/ApiConfig.js
+++ Frontend/src/config/ApiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL="http://localhost:8003"
+export const API_BASE_URL="https://e-commerce-website-11-uixe.onrender.com"
 const jwt=localStorage.getItem("jwt")
 
 
